cae29a99fe495a8a69142faa66cc2f53e0894910
Naty <148580617+NatyT777@users.noreply.github.com>
2023-10-27 16:01:12 -0300

    up

--- sktech.js
+++ sktech.js
@@ -1,13 +1,13 @@
 function setup() {
-    createCanvas(600, 600);
-   background("black");
+  createCanvas(600, 600);
+ background("black");
+}
+
+function draw() {
+ fill("lime");
+  stroke("blue")
+  strokeWeight(1)
+  if (mouseIsPressed){
+  rect(mouseX, mouseY, 20, 30);
   }
-  
-  function draw() {
-   fill("lime");
-    stroke("blue")
-    strokeWeight(1)
-    if (mouseIsPressed){
-    rect(mouseX, mouseY, 20, 30);
-    }
-  }
+}
